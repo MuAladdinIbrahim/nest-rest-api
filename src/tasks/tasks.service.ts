a4d4bc0fb821c861752de2799c8b6d97f590a251
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2023-04-04 14:17:57 +0200

    filter in get query

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,13 +3,27 @@ import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks() {
-    return this.tasks;
+  getTasks(filterDto: GetTasksFilterDto) {
+    const { status, search } = filterDto;
+    let tasks: Task[] = this.tasks;
+
+    if (status) {
+      tasks = tasks.filter((t) => t.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((t) => {
+        if (t.description.includes(search) || t.title.includes(search)) true;
+        else false;
+      });
+    }
+    return tasks;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
